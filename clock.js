1dbb54ff010cce019f06ebee8ebedfdaa64c8646
sarmadna <sarmad_n_a@hotmail.com>
2023-03-22 16:31:20 +0300

    update

--- clock.js
+++ clock.js
@@ -2,6 +2,7 @@ function currentTime() {
     let date = new Date();
     let hh = date.getHours();
     let mm = date.getMinutes();
+    let ss = date.getSeconds();
     let session = "AM";
 
     if (hh == 0) {
@@ -14,8 +15,9 @@ function currentTime() {
 
     hh = (hh < 10) ? "0" + hh : hh;
     mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
 
-    let time = hh + ":" + mm + " " + session;
+    let time = hh + ":" + mm + ":" + ss + " " + session;
 
     document.getElementById("clock").innerText = time;
     let t = setTimeout(function(){ currentTime() }, 1000);

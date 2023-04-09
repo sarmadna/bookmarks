d3cb3a5a8b6187b4afaac82c0d3fdb6b2a885457
sarmadna <sarmad_n_a@hotmail.com>
2023-04-10 00:14:03 +0300

    update

--- date.js
+++ date.js
@@ -1,12 +1,12 @@
 function currentDate() {
     let date = new Date();
-    let day = date.toLocaleString("default", {weekday: "long"});
     let dd = date.getDate();
     let mmm = date.toLocaleString("default", {month: "short"});
+    let yy = date.toLocaleString("default", {year: "2-digit"});
 
     dd = (dd < 10) ? "0" + dd : dd;
 
-    let today = day + ", " + mmm + " " + dd;
+    let today = dd + "-" + mmm + "-" + yy;
 
     document.getElementById("date").innerText = today;
     let t = setTimeout(function(){currentDate() }, 1000);

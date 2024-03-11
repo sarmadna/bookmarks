4bb71fc67c551c76c00909bf0529727bf20c29fb
sarmadna <sarmad_n_a@hotmail.com>
2024-03-11 06:00:36 +0300

    update

--- old1/dark.js
+++
@@ -1,7 +0,0 @@
-const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-var element = document.body;
-if (darkThemeMq.matches) {
-    element.classList.toggle("bg-dark");
-  } else {
-    element.classList.toggle("bg-light");
-  }
